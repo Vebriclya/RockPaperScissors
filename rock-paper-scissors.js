c8ea94d7b951ed10b5a05d224e3795bd2c8b9cb3
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-19 16:12:59 +0100

    Further refactoring of foreach l00p

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -28,7 +28,7 @@
      */
 
     // Loop through the array to calculate winners
-    winnerArray.forEach((winner) => {
+    winnerArray.forEach(winner => {
             if(winner === "pc"){
                 pcWins += 1;
             }
