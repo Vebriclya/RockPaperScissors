45a5a2c95c114bd403c7a773b04aa8251968b898
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-19 13:10:56 +0100

    Played around with keeping track of player vs computer wins

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,14 +1,23 @@
     // Greet user
     console.log("Welcome to Rock, Paper, Scissors!");
 
-    playRound();
+    let winner = playRound();
+    
+    if(winner === "pc"){
 
+    }
+    else if(winner === "player")
+
+
+    // ask the player if they want to keep going
+    // keep track of how many times player has won, and how many times computer has won
 
 
 
 /* ___________FUNCTIONS___________ */
 
 function playRound(){
+    let winner = "";
     // Ask user to choose rock, paper, or scissors
     // Check the entry is valid
     let playerTurn = getUserEntry();
@@ -18,6 +27,8 @@ function playRound(){
 
     // Compare the user entry and the computer entry
     battle(playerTurn, computerTurn);
+
+    return winner;
 }
 
 function generateComputerTurn(){
@@ -56,51 +67,64 @@ function getUserEntry(){
 }
 
 function battle(playerTurn, computerTurn){
+    let winner = "";
+     
     switch(true){
         // both values are the same = tie
         case (computerTurn === playerTurn):
             console.log(`You played ${playerTurn} vs computers ${computerTurn}`);
             console.log("It's a tie!");
+            winner = "tie";
             break;
         //player rock vs pc paper = pc wins
         case (playerTurn === "rock" && computerTurn === "paper"):
             console.log(`You played ${playerTurn} vs computers ${computerTurn}`);
             console.log("Computer wins!");
+            winner = "pc";
             break;
         //pc rock vs player paper = player wins
         case (computerTurn === "rock" && playerTurn === "paper"):
             console.log(`You played ${playerTurn} vs computers ${computerTurn}`);
             console.log("Player wins!");
+            winner = "player";
             break;
         //pc rock vs player scissors = pc wins
         case (computerTurn === "rock" && playerTurn === "scissors"):
             console.log(`You played ${playerTurn} vs computers ${computerTurn}`);
             console.log("Computer wins!");
+            winner = "pc";
             break;
         //player rock vs pc scissors = player wins
         case (playerTurn === "rock" && computerTurn === "scissors"):
             console.log(`You played ${playerTurn} vs computers ${computerTurn}`);
             console.log("Player wins!");
+            winner = "player";
             break;
         //pc paper vs player rock = pc wins
         case (computerTurn === "paper" && playerTurn === "rock"):
             console.log(`You played ${playerTurn} vs computers ${computerTurn}`);
             console.log("Computer wins!");
+            winner = "pc";
             break;
         //player paper vs pc rock = player wins
         case (playerTurn === "paper" && computerTurn === "rock"):
             console.log(`You played ${playerTurn} vs computers ${computerTurn}`);
             console.log("Player wins!");
+            winner = "player";
             break;
         //player paper vs pc scissors = pc wins
         case (playerTurn === "paper" && computerTurn === "scissors"):
             console.log(`You played ${playerTurn} vs computers ${computerTurn}`);
             console.log("Computer wins!");
+            winner = "pc";
             break;
         //pc paper vs player scissors = player wins
         case (computerTurn === "paper" && playerTurn === "scissors"):
             console.log(`You played ${playerTurn} vs computers ${computerTurn}`);
             console.log("Player wins!");
+            winner = "player";
             break;  
     }
+
+    return winner;
 }
